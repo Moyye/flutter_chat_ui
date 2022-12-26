378b70e298826ae69a1a1cfd2efe95c65ebbdac1
潘幸 <panxing@paxini.com>
2022-12-26 15:26:49 +0800

    feat: remove button safe area

--- lib/src/widgets/input/input.dart
+++ lib/src/widgets/input/input.dart
@@ -141,7 +141,7 @@ class _InputState extends State<Input> {
             query.padding.left,
             0,
             query.padding.right,
-            query.viewInsets.bottom + query.padding.bottom,
+            query.viewInsets.bottom,
           )
         : EdgeInsets.zero;
     final textPadding = InheritedChatTheme.of(context)
